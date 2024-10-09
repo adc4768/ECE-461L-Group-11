1c0417878a9def809a0a85797ce38056933dcc0d
tgeorge676 <timothy.george@utexas.edu>
2024-10-09 14:53:01 -0500

    Added hardwareSet class

---
+++ server/hardwareSet.py
@@ -0,0 +1,36 @@
+class hardwareSet:
+    def __init__(self):
+        self.__capacity = 0
+        self.__availability = 0
+        self.__checkedOut = [0] * 1000
+        self.__hwName = "foo"
+
+    def initialize_capacity(self,qty):
+        self.__capacity = qty
+        self.__availability = qty
+
+    def get_availability(self):
+        return self.__availability
+
+    def get_capacity(self):
+        return self.__capacity
+    def get_name(self):
+        return self.__hwName
+
+    def check_out(self,qty,projectID):
+        if qty > self.__availability: 
+            self.__checkedOut[projectID] = self.__availability
+            self.__availability = 0
+            return -1
+        self.__checkedOut[projectID] = self.__checkedOut[projectID] + qty
+        self.__availability = self.__availability - qty
+        return 0
+
+    def check_in(self,qty,projectID):
+        if qty > self.__checkedOut[projectID]:
+            return -1
+        self.__checkedOut[projectID] = self.__checkedOut[projectID] - qty
+        self.__availability = self.__availability + qty
+        return 0
+
+    
