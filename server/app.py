0ed453a1c72bf09f0155394f5b7c998c344a7c8f
Yuhi <yuhi.142857@akane.waseda.jp>
2024-11-08 14:43:20 -0600

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-# app.py
-
 # Import necessary libraries and modules
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
@@ -7,7 +5,6 @@ from pymongo import MongoClient
 # Import custom modules for database interactions
 import usersDatabase as usersDB
 import projectsDatabase as projectsDB
-import hardwareDatabase as hardwareDB
 
 # Define the MongoDB connection string
 MONGODB_SERVER = "mongodb+srv://Group11:LjIkXFETwAWP2YxP@cluster0.pg1fa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -108,61 +105,23 @@ def get_project_info():
     else:
         return jsonify({'message': 'Project not found.'}), 400
 
-# Route for getting all hardware set names
-@app.route('/get_all_hw_names', methods=['GET'])
-def get_all_hw_names():
-    # Connect to MongoDB
-    client = MongoClient(MONGODB_SERVER)
-
-    # Fetch all hardware set names using the hardwareDB module
-    hw_names = hardwareDB.getAllHwNames(client)
-
-    # Close the MongoDB connection
-    client.close()
-
-    # Return a JSON response
-    return jsonify({'hardwareSets': hw_names}), 200  
-
-# Route for getting hardware information
-@app.route('/get_hw_info', methods=['GET'])
-def get_hw_info():
-    # Extract 'hwSetName' from query parameters
-    hwSetName = request.args.get('hwSetName')
-
-    # Connect to MongoDB
-    client = MongoClient(MONGODB_SERVER)
-
-    # Fetch hardware set information using the hardwareDB module
-    hardware_set = hardwareDB.queryHardwareSet(client, hwSetName)
-
-    # Close the MongoDB connection
-    client.close()
-
-    # Return a JSON response
-    if hardware_set:
-        # Remove ObjectId from the response
-        hardware_set.pop('_id', None)
-        return jsonify({'hardwareSet': hardware_set}), 200  
-    else:
-        return jsonify({'message': 'Hardware set not found.'}), 400
-
 # Route for checking out hardware
 @app.route('/check_out', methods=['POST'])
 def check_out():
     # Extract data from request
     data = request.get_json()
+    projectId = data.get('projectId')
     hwSetName = data.get('hwSetName')
     qty = int(data.get('qty', 0))
-    projectID = data.get('projectId')
 
-    if not projectID:
+    if not projectId:
         return jsonify({'message': 'Project ID not provided.'}), 400
 
     # Connect to MongoDB
     client = MongoClient(MONGODB_SERVER)
 
-    # Attempt to check out the hardware using the hardwareDB module
-    success = hardwareDB.check_out(client, hwSetName, qty, projectID)
+    # Attempt to check out the hardware using the projectsDB module
+    success = projectsDB.checkOutHW(client, projectId, hwSetName, qty)
 
     # Close the MongoDB connection
     client.close()
@@ -178,18 +137,18 @@ def check_out():
 def check_in():
     # Extract data from request
     data = request.get_json()
+    projectId = data.get('projectId')
     hwSetName = data.get('hwSetName')
     qty = int(data.get('qty', 0))
-    projectID = data.get('projectId')
 
-    if not projectID:
+    if not projectId:
         return jsonify({'message': 'Project ID not provided.'}), 400
 
     # Connect to MongoDB
     client = MongoClient(MONGODB_SERVER)
 
-    # Attempt to check in the hardware using the hardwareDB module
-    success = hardwareDB.check_in(client, hwSetName, qty, projectID)
+    # Attempt to check in the hardware using the projectsDB module
+    success = projectsDB.checkInHW(client, projectId, hwSetName, qty)
 
     # Close the MongoDB connection
     client.close()
@@ -199,29 +158,6 @@ def check_in():
         return jsonify({'message': 'CHECK IN SUCCESS'}), 200  
     else:
         return jsonify({'message': 'CHECK IN FAIL'}), 400
-
-# Route for creating a new hardware set
-@app.route('/create_hardware_set', methods=['POST'])
-def create_hardware_set():
-    # Extract data from request
-    data = request.get_json()
-    hwSetName = data.get('hwSetName')
-    initCapacity = int(data.get('initCapacity', 100))
-
-    # Connect to MongoDB
-    client = MongoClient(MONGODB_SERVER)
-
-    # Attempt to create the hardware set using the hardwareDB module
-    success = hardwareDB.createHardwareSet(client, hwSetName, initCapacity)
-
-    # Close the MongoDB connection
-    client.close()
-
-    # Return a JSON response
-    if success:
-        return jsonify({'message': 'Hardware set created successfully.'}), 200
-    else:
-        return jsonify({'message': 'Hardware set creation failed. It may already exist.'}), 400  
 
 # Main entry point for the application
 if __name__ == '__main__':
